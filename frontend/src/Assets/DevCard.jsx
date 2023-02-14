c0776ab37adc04f0a60cc39bee6b5795588b60f7
Seth Parsons <seth.parsons007@gmail.com>
2023-02-13 23:19:30 -0600

    added developer cards to about me. partially working

---
+++ frontend/src/Assets/DevCard.jsx
@@ -0,0 +1,35 @@
+import Card from 'react-bootstrap/Card';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+const DevCard = (inputUser) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px180" />
+        <Tabs
+        defaultActiveKey="about"
+        id="devcard-tabs"
+        className="mb-3"
+        justify
+        >
+            <Tab eventKey="about" title="About">
+                <Card.Body>
+                        <Card.Title>{inputUser.inputUser.name}</Card.Title>
+                        <Card.Text>Role:</Card.Text>
+                        <Card.Text>{inputUser.inputUser.bio}</Card.Text>
+                </Card.Body>
+            </Tab>
+            <Tab eventKey="GitLab" title="GitLab"> 
+                <Card.Body>
+                    <Card.Title>@{inputUser.inputUser.gitlab}<br /></Card.Title>
+                    Commits: {inputUser.inputUser.commits}<br />
+                    Issues: {inputUser.inputUser.issues}<br />
+                    Unit Tests: {inputUser.inputUser.tests}
+                </Card.Body>
+            </Tab>
+        </Tabs>
+    </Card>
+  );
+}
+
+export default DevCard;
