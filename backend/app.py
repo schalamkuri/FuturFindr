52f1968f5f2509c3fd5701e02a623574307d4ff7
Seth Parsons <seth_parsons@ats-engineers.com>
2023-03-04 18:58:32 -0600

    now also supports api calls for seeing only singular model instances

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+# This app is inspired by GeoJobs's implementation. Their repo is here:
+# https://gitlab.com/sarthaksirotiya/cs373-idb
+
 from flask import Flask, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text, column, desc
-from models import app, db, Job, College, HousingUnit
+from models import app, db, Job, College, HousingUnit, HousingUnitImage
 from schema import job_schema, housing_unit_schema, college_schema, housing_unit_img_schema, college_img_schema
 import json
 
@@ -13,6 +16,9 @@ if __name__ == "__main__":
 @app.route("/")
 def home():
     return "<h1>FuturFindr API</h1>"
+
+
+# Functions for returning lists of models
 
 @app.route("/colleges")
 def get_colleges():
@@ -63,9 +69,41 @@ def get_jobs():
     return jsonify({"data": result, "meta": {"count": count}})
 
 
+# Functions for returning singular instances of models
+
+@app.route("/colleges/<int:instance_id>")
+def get_college(instance_id):
+    college = College.query.filter_by(id=instance_id).first()
+    if college == None:
+        return Response(json.dumps({"error": "Invalid college ID"}), mimetype="application/json")
+    result = college_schema.dump(college)
+    college_images = college_img_schema.dump(college.images, many=True)
+    result.update({"images": college_images})
+    return jsonify({"data": result})
+
+@app.route("/housing/<string:instance_id>")
+def get_housing_unit(instance_id):
+    house = HousingUnit.query.filter_by(id=instance_id).first()
+    if house == None:
+        return Response(json.dumps({"error": "Invalid housing unit ID"}), mimetype="application/json")
+    result = housing_unit_schema.dump(house)
+    housing_images = HousingUnitImage.query.filter_by(housing_id=instance_id).first()
+    image_dump = housing_unit_img_schema.dump(housing_images)
+    result.update({"images": image_dump})
+    return jsonify({"data": result})
+
+@app.route("/jobs/<int:instance_id>")
+def get_job(instance_id):
+    job = Job.query.filter_by(id=instance_id).first()
+    if job == None:
+        return Response(json.dumps({"error": "Invalid job ID"}), mimetype="application/json")
+    result = job_schema.dump(job)
+    return jsonify({"data": result})
+
+
+
 def paginate_helper(page_num, per_page, query):
-    if per_page is None:
-        per_page = DEFAULT_PAGE_SIZE
-    return query.paginate(page=page_num, per_page=per_page, error_out=False).items
+    return query.paginate(page=page_num, per_page=(DEFAULT_PAGE_SIZE if per_page is None else per_page), \
+                          error_out=False).items
 
 
