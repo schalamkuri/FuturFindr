979448414c5001b3a2d3313fe27ff5ef878dc528
Saiprathik Chalamkuri <saiprathikchalamkuri@wireless-10-145-10-115.public.utexas.edu>
2023-04-05 13:33:56 -0500

    jobs instance has embeded map

--- frontend/src/Views/visualizations.jsx
+++ frontend/src/Views/visualizations.jsx
@@ -4,17 +4,7 @@ function Visualizations() {
     <Typography>
       
       <div>
-      {/* <h1>Visualizations</h1> */}
-       <iframe
-       
-  width="600"
-  height="450"
-  style={{ border: "0" }}
-  loading="lazy"
-  allowfullscreen
-  referrerpolicy="no-referrer-when-downgrade"
-  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyA9kS5d3pbtHba18DDfLWZkAxDOe6N04EY&q=Space+Needle,Seattle+WA`}>
-</iframe>
+      <h1>Visualizations</h1>
     </div>
     </Typography>
 
